d0a5d4a220706d8e7ac1f0b5f5f8fdc3c131d7c4
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-09-10 11:19:50 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useState, useRef } from "react";
 import TodoList from "./TodoList";
+import uuidv4 from "uuid/v4";
 
 function App() {
-  return <TodoList />;
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
+    });
+    todoNameRef.current.value = null;
+  }
+  return (
+    <>
+      <TodoList />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button>Clear Complete</button>
+      <div>0 left to do</div>
+    </>
+  );
 }
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function TodoList() {
-  return <div></div>;
+  return <div>Hello World</div>;
 }
